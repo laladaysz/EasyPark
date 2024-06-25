e0377bf91f03ff9a8add73291c87f82770f7c130
laladaysz <lauraleandra.dias@gmail.com>
2024-06-25 11:24:46 -0300

    :sparkles: feat: frames vagas ocupadas

--- vaga/parking_space.py
+++ vaga/parking_space.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 
+# from LPR.main import PlateRecognition
+
 def ParkingSpace():
     vaga1 = [1, 89, 108, 213]
     vaga2 = [115, 87, 152, 211]
@@ -11,13 +13,17 @@ def ParkingSpace():
     vaga7 = [881, 93, 138, 201]
     vaga8 = [1027, 94, 147, 202]
 
-    vagas = [vaga1, vaga2, vaga3, vaga4, vaga4, vaga5, vaga6, vaga7, vaga8]
+    vagas = [vaga1, vaga2, vaga3, vaga4, vaga5, vaga6, vaga7, vaga8]
 
     video = cv2.VideoCapture(r'C:\Users\ct67ca\Desktop\Easy_Park\videos\vagas.mp4')
     check = True
+    # qPLR = PlateRecognition(r'C:\Users\ct67ca\Desktop\Easy_Park\videos\placas.mp4')
+    
+    capturado = [False] * len(vagas)
 
     while check == True:
         check,img = video.read() 
+        
         
         if not check:
             break
@@ -30,19 +36,24 @@ def ParkingSpace():
         imgDil = cv2.dilate(imgBlur, kernel)
         mask = np.zeros_like(img)
 
-        # desenhando quadradinhos verdes sobre a img
-        for x, y, w, h in vagas:
+        # desenhando quadradinhos verdes sobre a img 
+        for i, (x, y, w, h) in enumerate(vagas):
             cv2.rectangle(mask, (x, y), (x + w, y + h), (255, 255, 255), -1)
             recorte = imgDil[y:y+h,x:x+w]
             qtPxBranco = cv2.countNonZero(recorte)
             cv2.putText(img,str(qtPxBranco),(x,y+h-10),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
-        
-            if qtPxBranco > 3000:
+            
+            if qtPxBranco > 4500 and not capturado[i]:
                 cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 2)
-                
+            
+                cv2.imwrite(f"frame_vaga{i+1}.jpg", img)
+                capturado[i] = True
+                print (f"capturado com sucesso! {i+1}")              
+                continue
     
             else:
                 cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+  
 
         masked_img = cv2.bitwise_and(img, mask)
         cv2.imshow('video', masked_img)
